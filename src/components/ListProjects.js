e4233da848b324cc53bfbe9d94b8df0d7dba2522
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2022-07-31 21:12:42 -0600

    Update ListProjects.js

--- src/components/ListProjects.js
+++ src/components/ListProjects.js
@@ -1,11 +1,9 @@
-import DenoteNote from './DenoteNote';
-import AddNotes from './AddNotes';
-const ListNotes = ({ theNotes, addtheNote, removetheNote, setNotes, }) => {
+import DenoteProject from './DenoteProject';
+const ListProjects = ({ theNotes, addtheNote, removetheNote, setNotes, }) => {
   return (
     <div className='listnotes'>
     {theNotes.map((note)=>   <DenoteNote theNotes={theNotes}noteId={note.noteId} noteText={note.noteText} noteDate={note.noteDate}noteColor={note.noteColor}removetheNote={removetheNote} setNotes={setNotes} />)}
-  <AddNotes addtheNote={addtheNote}/>
 </div>
     );
 };
-export default ListNotes;
+export default ListProjects;
