5f14a574d7131924c47cac38cb56bb58c4bd541f
RyanGormican <ryan.gormican@gmail.com>
2023-03-14 03:32:29 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Grid from '@mui/material/Grid';
 import Tooltip from '@mui/material/Tooltip';
 import { Icon } from '@iconify/react';
 import Compscidle from './images/Compscidle.png'
+import Denoted from './images/Denoted.png'
+import Listlast from './images/Listlast.png'
+import Quotedfor from './images/Quotedfor.png'
+import Moviesight from './images/Moviesight.png'
+import Realdiscuss from './images/Realdiscuss.png'
 function App() {
   return (
     <div className="App" >
@@ -13,41 +18,51 @@ function App() {
 			<a href="https://www.linkedin.com/in/ryangormican/">
 				<Icon icon="mdi:linkedin" color="#0e76a8" width="40" />
 			</a>
-			<a href="https://github.com/RyanGormican/RealDiscuss">
+			<a href="https://github.com/RyanGormican/">
 				<Icon icon="mdi:github" color="#e8eaea" width="40" />
 			</a>
 	    </div>
       </Container>
      <Grid container spacing={0.5}>
         <Grid item xs={4}>
-            <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="85%">
-                </img>
-            </a>
+             <Tooltip title="RealDiscuss is a full-stack web application developed using the Next.js framework for the React library. RealDiscuss is a message board app allowing the users after verifying user authentication to manage posts. Users can manage their own posts from the Google Firebase database by either creating a new one or deleting and editing the ones they already have available to them. Users can also manage posts by clicking on a specific post on the index page and on the comments section allowing the user to add comments to specific users' posts. ">
+                <a href = "https://realdiscuss.vercel.app/">
+                    <img src = {Realdiscuss} width="85%">
+                    </img>
+                </a>
+            </Tooltip>
         </Grid> 
        <Grid item xs={4}>
-            <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="85%">
-                </img>
-            </a>
+              <Tooltip title="QuotedFor is a web app made using the React library. The web app provides for the user to view a thought-provoking quote on load and through the push of a button. The application for retrieving the quotes for the web app was fetched using API calls.">
+                <a href = "https://quotedfor.vercel.app/">
+                    <img src = {Quotedfor} width="85%">
+                    </img>
+                </a>
+            </Tooltip>
         </Grid> 
        <Grid item xs={4}>
-            <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="85%">
-                </img>
-            </a>
+               <Tooltip title="MovieSight is a front-end web app created with the Vue framework. The application allows the user to view details on the latest popular movies. With the integration of API calls, the user gets displayed on the home page a featured movie along with a list of movies and their descriptions. Users can then click on either the view more for the featured movie or just on the movie itself for the list of movies and get taken to a vision page in order to gain more details about the movie such as its runtime and the country of origin for the film. ">
+                <a href = "https://moviesight.vercel.app/">
+                    <img src = {Moviesight} width="85%">
+                    </img>
+                </a>
+            </Tooltip>
         </Grid> 
         <Grid item xs={4}>
-            <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="85%">
-                </img>
-            </a>
+             <Tooltip title="ListLast is a to-do list app allowing the user to add, update, and remove tasks from a list. The Bootstrap framework, and React states are utilized in order for users to be able to manage those actions through the buttons on the interface.">
+                <a href = "https://listlast.vercel.app/">
+                    <img src = {Listlast} width="85%">
+                    </img>
+                </a>
+            </Tooltip>
         </Grid> 
        <Grid item xs={4}>
-            <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="85%">
-                </img>
-            </a>
+             <Tooltip title="DeNoted is a notes app developed using React and an assortment of web technologies. Users are able to manage the notes on the page by adding and deleting the notes they want to change. Management of notes also allows the user to change the design of a note by modifying the background color.">
+                <a href = "https://denoted.vercel.app/">
+                    <img src = {Denoted} width="85%">
+                    </img>
+                </a>
+            </Tooltip>
         </Grid> 
        <Grid item xs={4}>
             <Tooltip title="CompScidle is a clone of the popular New York Times game Wordle using the React library. The user has 6 attempts to guess a 5 letter word using the keyboard onscreen or by typing on the keyboard. Words are pulled from a .txt file all centered around program and computer science-related topics.">
