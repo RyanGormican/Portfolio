8473bf45d5ccdac02b8997084980289adfc5efcc
RyanGormican <ryan.gormican@gmail.com>
2023-03-14 02:49:27 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import { Icon } from '@iconify/react';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Container maxWidth="sm">
+         <div className="flex">
+			<a href="https://www.linkedin.com/in/ryangormican/">
+				<Icon icon="mdi:linkedin" color="#0e76a8" width="40" />
+			</a>
+			<a href="https://github.com/RyanGormican/RealDiscuss">
+				<Icon icon="mdi:github" color="#e8eaea" width="40" />
+			</a>
+	    </div>
+      </Container>
      <Grid container spacing={0.5}>
         <Grid item xs={4}>
         a
@@ -27,7 +37,6 @@ function App() {
         </Grid> 
      </Grid>
 
-      </header>
     </div>
   );
 }
