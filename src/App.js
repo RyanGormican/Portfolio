031b2ce1cfb90f2984ca2aab7380a577be7d418d
RyanGormican <ryan.gormican@gmail.com>
2023-03-14 03:12:38 -0600

    a
    a

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
+import Tooltip from '@mui/material/Tooltip';
 import { Icon } from '@iconify/react';
 import Compscidle from './images/Compscidle.png'
 function App() {
@@ -26,33 +27,35 @@ function App() {
         </Grid> 
        <Grid item xs={4}>
             <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="70%">
+                <img src = {Compscidle} width="85%">
                 </img>
             </a>
         </Grid> 
        <Grid item xs={4}>
             <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="70%">
+                <img src = {Compscidle} width="85%">
                 </img>
             </a>
         </Grid> 
         <Grid item xs={4}>
             <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="70%">
+                <img src = {Compscidle} width="85%">
                 </img>
             </a>
         </Grid> 
        <Grid item xs={4}>
             <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="70%">
+                <img src = {Compscidle} width="85%">
                 </img>
             </a>
         </Grid> 
        <Grid item xs={4}>
-            <a href = "https://compscidle.vercel.app/">
-                <img src = {Compscidle} width="70%">
-                </img>
-            </a>
+            <Tooltip title="CompScidle is a clone of the popular New York Times game Wordle using the React library. The user has 6 attempts to guess a 5 letter word using the keyboard onscreen or by typing on the keyboard. Words are pulled from a .txt file all centered around program and computer science-related topics.">
+                <a href = "https://compscidle.vercel.app/">
+                    <img src = {Compscidle} width="85%">
+                    </img>
+                </a>
+            </Tooltip>
         </Grid> 
      </Grid>
 
