027a523cd05b4577c2e4090aa4b97275537e47ce
RyanGormican <ryan.gormican@gmail.com>
2023-08-26 02:07:04 -0600

    a
    a

--- src/backend/db.js
+++ src/backend/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   database: process.env.POSTGRES_DATABASE,
   password: process.env.POSTGRES_PASSWORD,
   port: process.env.POSTGRES_PORT, 
+   connectionString: process.env.POSTGRES_URL,
 });
 
 module.exports = pool;
--- src/backend/index.js
+++ src/backend/index.js
@@ -7,9 +7,25 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/app/suggestion', suggestionsRoutes);
+app.use('/app/suggestions', suggestionsRoutes);
 
-const PORT = process.env.port || 5000;
+const pool = require('./db');
+
+const PORT = process.env.POSTGRES_PORT || 5000;
 app.listen(PORT, () => {
+  console.log("Server is listening on port", PORT);
+
+  app.get('/test-db-connection', (req, res) => {
+  const query = 'SELECT NOW()';
+  pool.query(query, (err, result) => {
+    if (err) {
+      console.error('Error testing database connection:', err);
+      res.status(500).json({ error: 'Error connecting to the database' });
+    } else {
+      console.log('Connected to the database at:', result.rows[0].now);
+      res.json({ message: 'Connected to the database', timestamp: result.rows[0].now });
+    }
+  });
+});
 
 });
