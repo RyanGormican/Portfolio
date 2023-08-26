be8d1dc07289ca9959be5a6a786bd15488615a3f
RyanGormican <ryan.gormican@gmail.com>
2023-08-26 02:19:25 -0600

    a
    a

--- src/backend/index.js
+++ src/backend/index.js
@@ -12,20 +12,8 @@ app.use('/app/suggestions', suggestionsRoutes);
 const pool = require('./db');
 
 const PORT = process.env.POSTGRES_PORT || 5000;
+
 app.listen(PORT, () => {
   console.log("Server is listening on port", PORT);
-
-  app.get('/test-db-connection', (req, res) => {
-  const query = 'SELECT NOW()';
-  pool.query(query, (err, result) => {
-    if (err) {
-      console.error('Error testing database connection:', err);
-      res.status(500).json({ error: 'Error connecting to the database' });
-    } else {
-      console.log('Connected to the database at:', result.rows[0].now);
-      res.json({ message: 'Connected to the database', timestamp: result.rows[0].now });
-    }
-  });
-});
 
 });
