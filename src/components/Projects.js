8b384a4ffb041af5690235dd870bd118859dbbf5
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2023-12-27 05:20:37 -0600

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,7 @@ import CalculateExpress from '../images/CalculateExpress.png'
 import TriviaTempo from '../images/TriviaTempo.png'
 import PaintersParadise from '../images/PaintersParadise.png'
 import SimplyCharts from '../images/SimplyCharts.png'
+import WordWonder from '../images/WordWonder.png'
 import Grid from '@mui/material/Grid';
 import {Tag} from 'antd';
 import {analytics} from '../firebaseConfig.js';
@@ -43,6 +44,7 @@ switch(tag){
 
 };
     const projects = [
+	{name: WordWonder, title:"WordWonder" , description: WordWonder is a front-end word-processing app done with React.js. Users can use their textual input along with the given set of tools to produce a document for them to use.", link: "https://wordwonder.vercel.app/"},
 	{name: SimplyCharts, title:"SimplyCharts" , description: "SimplyCharts is a lightweight React.js web application designed for generating charts. Users can use the onscreen controls to customize their charts. After pressing the button, the chart image gets generated using the QuickChart API.", link: "https://simplycharts.vercel.app/"},
 	{name: PaintersParadise, title:"PaintersParadise" , description: "PaintersParadise is a drawing app constructed using Vue.js. Users are able to construct their masterpiece on the canvas using the given design tools.", link: "https://paintersparadise.vercel.app/"},
         {name: TriviaTempo, title:"TriviaTempo" , description: "TriviaTempo is a fast-paced web-based trivia game built with React.js. Using The Trivia API, users are given a rapid fire of trivia questions from a variety of categories with 15 seconds to answer each one.", link: "https://triviatempo.vercel.app/"},
