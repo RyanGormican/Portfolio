1118ef43dc32cbfb4f7ead310331e394180d15ab
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2024-04-01 05:44:12 -0600

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -47,7 +47,7 @@ switch(tag){
 }
 };
     const projects = [
-	{name: Searchimization, title:"Searchimization" , description: "Searchimization is a Next.js web application. Inspired by the NYT game of 'Strands', users can create their word search puzzles and share them for people to play.", link: "https://searchimization.vercel.app/"},
+	{name: Searchimization, title:"Searchimization" , description: "Searchimization is a Next.js web application. Users can create their word search puzzles and share them for people to play.", link: "https://searchimization.vercel.app/"},
 	{name: IdleCubicle, title:"IdleCubicle" , description: "IdleCubicle is a React.js idle game. Users manage a multitude of resources while completing objectives and milestones along the way.", link: "https://idlecubicle.vercel.app/"},
 	{name: KanbanKeeper, title:"KanbanKeeper" , description: "KanbanKeeper is a Vue.js web application. Users are able to keep track and manage their Kanban board by controlling the various properties pertaining to its cards and lists.", link: "https://kanbankeeper.vercel.app/"},
 	{name: Pokendex, title:"Pokendex" , description: "(HOSTING CURRENTLY UNAVAILABLE) Pokendex is a Ruby on Rails app acting as a web-based Pokedex. Users can view and examine information regarding the 1025 Pokemon that exist within the series.", link: "https://github.com/RyanGormican/Pokendex"},
